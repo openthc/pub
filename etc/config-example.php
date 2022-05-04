750f709cd30c7fbfa4ed46349ba7d956164f1999
openthc <code@openthc.com>
2022-05-04 02:57:38 +0000

    add example configs

---
+++ etc/config-example.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * OpenTHC Pub Configuration
+ */
+
+$ret = [];
+
+$ret['app'] = [
+	'base' => 'https://pub.openthc.example.com/'
+];
+
+$ret['pub'] = [
+	'public' => '',
+	'secret' => '',
+];
+
+return $ret;
