76beee410e3ed0b9e760cdfbc02eb14a9d6d08c8
openthc <code@openthc.com>
2024-08-15 21:49:38 +0000

    Update Config Example

--- etc/config-example.php
+++ etc/config-example.php
@@ -5,11 +5,18 @@
 
 $ret = [];
 
-$ret['app'] = [
-	'base' => 'https://pub.openthc.example.com/'
+$ret['database'] = [
+	'pub' => [
+		'hostname' => 'localhost',
+		'username' => 'openthc_pub',
+		'password' => 'openthc_pub',
+		'database' => 'openthc_pub',
+	]
 ];
 
-$ret['pub'] = [
+$ret['openthc'] = [];
+$ret['openthc']['pub'] = [
+	'origin' => 'https://pub.openthc.example',
 	'public' => '',
 	'secret' => '',
 ];
